f207ee778e424eb0249fd3daa0feea37da4dc065
Jelle Besseling <jellebesseling@gmail.com>
2016-08-26 18:28:19 +0200

    Added helper.py tests

---
+++ tests/test_helper.py
@@ -0,0 +1,15 @@
+import pytest
+
+class MockApp():
+    async def sendMessage(*args, **kwargs):
+        return (args, kwargs)
+
+@pytest.fixture
+def sender():
+    from tlask.helper import Sender
+    update = {'message': {'chat': {'id': 'test'}}}
+    return Sender(MockApp(), update)
+
+@pytest.mark.async
+async def test_sender_send(sender):
+    sender.send("Test")
